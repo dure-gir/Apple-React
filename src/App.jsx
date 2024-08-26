f9950629bff9d529c191189caa7183e9278ceaee
Dura1617 <duragirma@gmail.com>
2024-08-26 05:15:12 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Header from "./commonResource/components/Header";
+
+import Alert from "./commonResource/components/Alert";
+
+import FirstSection from "./commonResource/components/FirstSection";
+
+import SecondSection from "./commonResource/components/SecondSection";
+
+import ThirdSection from "./commonResource/components/ThirdSection";
+
+import FourthSection from "./commonResource/components/FourthSection";
+
+import FifthSection from "./commonResource/components/FifthSection";
+
+import SixthSection from './commonResource/components/SixthSection'
+
+import Footer from "./commonResource/components/Footer";
+
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Alert />
+			<FirstSection />
+			<SecondSection />
+			<ThirdSection />
+			<FourthSection />
+            <FifthSection />
+            <SixthSection />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/commonResource/components/FooterCollapes.js
@@ -0,0 +1,14 @@
+// import $ from "jquery";
+// (document).onabort("click", ".footer-link-wrapper h3", function () {
+//     console.log("Hello")
+//     if ($(window).width() <= 768) {
+//         $(this).toggleClass("expanded")
+//         $(this).next().slideToggle()
+//     } else {
+//         $(this).next('ul').show()
+//     }
+// })
+
+// $(window).on("resize", (function (){
+//     window.location.reload()
+// }))
